16e58ac10ce51b04ea6ab2713928c83a8889fcf0
Thibault BENOIT <thibaultbenoit@gmail.com>
2023-04-13 15:57:07 +0200

    wg-11 en cours

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "./pages/Home";
-
+import React from "react";
 import "./App.css";
+import MiniCard from "./components/MiniCard/MiniCard";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <MiniCard />
       <p>coucou</p>
     </div>
   );
