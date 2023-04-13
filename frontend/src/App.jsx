fdd72145d47566fe830b88ef614d68bce43c7612
kaezersoze <fingerzjbm@gmail.com>
2023-04-13 15:53:20 +0200

    WG12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import Home from "./pages/Home";
-
-import "./App.css";
+import React from "react";
+import ConsoleButton from "./components/ConsoleButton/ConsoleButton";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div>
+      <h1>CONSOLE : </h1>
+      <div>
+        <ConsoleButton nom="PlayStation 4" classe="bouton-ps4" />
+      </div>
+      <ConsoleButton nom="PlayStation 5" classe="bouton-ps5" />
+      <ConsoleButton nom="Xbox" classe="bouton-xbox" />
+      <ConsoleButton nom="PC" classe="bouton-pc" />
+      <ConsoleButton nom="Switch" classe="bouton-switch" />
+      <ConsoleButton nom="Game boy" class="bouton-Game-boy" />
+      <h2>GENRE :</h2>
+      <div>
+        <ConsoleButton nom="Aventure" classe="bouton-aventure" />
+        <ConsoleButton nom="Action" classe="bouton-action" />
+        <ConsoleButton nom="RPG" classe="bouton-RPG" />
+        <ConsoleButton nom="FPS" classe="bouton-FPS" />
+        <ConsoleButton nom="Sport" classe="bouton-sport" />
+      </div>
     </div>
   );
 }
