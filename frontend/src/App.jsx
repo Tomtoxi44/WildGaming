ef3114cbf2d6db89be1f8178afc4198c16f36e19
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-04-20 15:43:20 +0200

    push du WG-14 Ajout des description des cartes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ConsoleButton from "./components/ConsoleButton/ConsoleButton";
 import NavBar from "./components/navBar/NavBar";
 import HeroImage from "./components/navBar/HeroImage";
 import Footer from "./components/footer/Footer";
-import "./App.css";
 import MiniCard from "./components/MiniCard/MiniCard";
+import CardDescription from "./components/CardDescription/CardDescription";
+import "./App.css";
 import "./index.scss";
 
 function App() {
@@ -21,32 +21,19 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
 
+  const [description, setDescription] = useState(undefined);
+
   return (
     <div className="App">
       <NavBar />
       <HeroImage />
-      <MiniCard cards={cards} />
-
-      <div>
-        <h1>CONSOLE : </h1>
-      </div>
-      <div>
-        <ConsoleButton nom="PlayStation 4" classe="bouton-ps4" />
-      </div>
-      <ConsoleButton nom="PlayStation 5" classe="bouton-ps5" />
-      <ConsoleButton nom="Xbox" classe="bouton-xbox" />
-      <ConsoleButton nom="PC" classe="bouton-pc" />
-      <ConsoleButton nom="Switch" classe="bouton-switch" />
-      <ConsoleButton nom="Game boy" classe="bouton-Game-boy" />
-      <h2>GENRE :</h2>
-      <div>
-        <ConsoleButton nom="Aventure" classe="bouton-aventure" />
-        <ConsoleButton nom="Action" classe="bouton-action" />
-        <ConsoleButton nom="RPG" classe="bouton-RPG" />
-        <ConsoleButton nom="FPS" classe="bouton-FPS" />
-        <ConsoleButton nom="Sport" classe="bouton-sport" />
-      </div>
-
+      <MiniCard cards={cards} key={cards.id} setDescription={setDescription} />
+      {description && (
+        <CardDescription
+          description={description}
+          setDescription={setDescription}
+        />
+      )}
       <Footer />
     </div>
   );
--- frontend/src/components/ConsoleButton/ConsoleButton.jsx
+++ frontend/src/components/ConsoleButton/ConsoleButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./ConsoleButton.css";
 
 function ConsoleButton({ nom, classe }) {
   function handleClick() {
