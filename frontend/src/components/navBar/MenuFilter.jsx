64e153b45cdb92eb4c5f4e267aedfed953164b36
thibault <thibaultbenoit0@gmail.com>
2023-04-26 15:38:54 +0200

    ajout de la searchbar dans menufilter

--- frontend/src/components/navBar/MenuFilter.jsx
+++ frontend/src/components/navBar/MenuFilter.jsx
@@ -1,4 +1,21 @@
-function MenuFilter() {
-  return <div className="MenuFilters">Coucou !</div>;
+import PropTypes from "prop-types";
+import SearchBar from "../SearchBar/SearchBar";
+
+function MenuFilter({ cards, searchTerm, setSearchTerm }) {
+  return (
+    <div className="MenuFilters">
+      <SearchBar
+        cards={cards}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+    </div>
+  );
 }
 export default MenuFilter;
+
+MenuFilter.propTypes = {
+  cards: PropTypes.string.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.string.isRequired,
+};
