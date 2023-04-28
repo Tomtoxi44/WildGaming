0d7ee69ddbeac86fb7ef497e09ce7a53233d6c9c
thibault <thibaultbenoit0@gmail.com>
2023-04-28 10:45:17 +0200

    hero corrigé

--- frontend/src/components/navBar/HeroImage.jsx
+++ frontend/src/components/navBar/HeroImage.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import React from "react";
-import img from "../../assets/heroImg.png";
 import MenuFilter from "./MenuFilter";
 
 function HeroImage({ showMenu }) {
   return (
     <div className="hero-container">
       {showMenu && <MenuFilter />}
-      <img src={img} alt="heros" className="hero-image" />
+      <img
+        src="https://i.jeuxactus.com/datas/divers/d/i/divers-jeux-video/xl/divers-jeux-video-artwor-5ec3efe21e0d1.jpg"
+        alt="heros"
+        className="hero-image"
+      />
     </div>
   );
 }
